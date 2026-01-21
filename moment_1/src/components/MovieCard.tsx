5d43fe6b9c55a773fa0cb29f1b7dced960ac5016
Ramona <rare2400@student.miun.com>
2026-01-21 12:43:51 +0100

    skapar en MovieCard-komponent med props och logik

--- moment_1/src/components/MovieCard.tsx
+++ moment_1/src/components/MovieCard.tsx
@@ -1,5 +1,25 @@
+// Interface för MovieCard-komponenten
 interface MovieCardProps {
     name: string,
     year: number,
     watched: boolean
 }
+
+// MovieCard-komponent som tar emot props enligt interfacet
+function MovieCard({ name, year, watched }: MovieCardProps) {
+
+    // Omvandlar boolean till text
+    const watchedText = watched ? "Ja" : "Nej"
+
+    // Returnerar sektion med namn, utgivningsår och om filmen är sedd eller inte
+    return (
+        <section className="movie-card">
+            <h2>{name}</h2>
+            <p>Utgivningsår: {year}</p>
+            <p>Sett: {watchedText}</p>
+        </section>
+    )
+}
+
+// Exporterar komponenten
+export default MovieCard

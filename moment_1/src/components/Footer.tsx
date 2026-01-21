4451c53de8b3cc111969b951a0e472503643c79f
Ramona <rare2400@student.miun.com>
2026-01-21 12:49:56 +0100

    skapar Footer-komponent

---
+++ moment_1/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+// Importerar CSS-fil
+import "./Footer.css"
+
+// Footer-komponent
+function Footer() {
+    return (
+        <footer>
+            <p>Laborationsuppgift för kursen DT207G, <br />
+                Backend-baserad webbutveckling <br />
+                Copyright &copy; Ramona Reinholdz</p>
+        </footer>
+    )
+}
+
+// Exporterar komponenten
+export default Footer 
